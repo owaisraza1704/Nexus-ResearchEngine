f28b2ec37d6b645014031dfe0283f20085d22305
RazaNaqsh <owaisraza1704@gmail.com>
2026-09-20 13:09:33 +0530

    chore: remove premature vector migration dependency

--- alembic/versions/0001_initial_workspace.py
+++ alembic/versions/0001_initial_workspace.py
@@ -18,7 +18,6 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.execute("CREATE EXTENSION IF NOT EXISTS vector")
     op.create_table(
         "workspaces",
         sa.Column("id", postgresql.UUID(as_uuid=True), nullable=False),
